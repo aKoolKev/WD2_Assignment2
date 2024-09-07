caad4db36f1609389d7d30cd61e03c1ffeb6cbdb
aKoolKev <kevindong0212@gmail.com>
2024-09-07 16:24:01 -0500

    Finalize code.

--- script.js
+++ script.js
@@ -1,56 +1,22 @@
-// // global var
-// let sidebarVisible = false;
 
-
-
-// function toggleSideBar(){
-
-//     //get the side bar element
-//     let sideBar_el = document.getElementById('tablet-side-bar');
-    
-//     //
-
-    
-//     if(sidebarVisible){
-//         //hide sidebar
-//         sideBar_el.classList.remove('show-side-bar');
-//         sideBar_el.classList.add('hide-side-bar');
-//         //show hamburger button
-//         //remove hid button
-//         alert('hide');
-//     }else{
-//         //show sidebar
-//         sideBar_el.classList.remove('hide-side-bar');
-//         sideBar_el.classList.add('show-side-bar');
-//         alert('show');
-//     }
-
-//     sidebarVisible = !sidebarVisible;
-// }
-
-
-
-
-function hideSideBar(){
-    //get the side bar element
+// Applies the 'hide-side-bar' css style to the mobile side bar to "hide" it
+function hideSidebar(){
+    //Get the mobile sidebar element
     let sideBar_el = document.getElementById('mobile-side-bar');
     sideBar_el.classList.remove('show-side-bar');
     sideBar_el.classList.add('hide-side-bar');
 }
 
-function showSideBar(){
-    //get the side bar element
+// Applies the 'show-side-bar' css style to the mobile side bar to "show" it
+function showSidebar(){
+    //Get the mobile sidebar element
     let sideBar_el = document.getElementById('mobile-side-bar');
     sideBar_el.classList.remove('hide-side-bar');
     sideBar_el.classList.add('show-side-bar');
 }
 
-
-
-
-
 window.onload = function(){
-    //grab the show hamburger menu button and add an event listner to it
-    let hamburgerMenu_buttonEl = document.getElementById('hamburger-menu-button').addEventListener('click', showSideBar);
-    let hideSideBar_buttonEl = document.getElementById('hide-sidebar-button').addEventListener('click', hideSideBar);
+    //Grab the buttons and when clicked, run their respective function call when clicked
+    document.getElementById('hamburger-menu-button').addEventListener('click', showSidebar);
+    document.getElementById('hide-sidebar-button').addEventListener('click', hideSidebar);
 }
